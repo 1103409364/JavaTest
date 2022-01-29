9a454fd0b49270dfa25b8b0f7f3d1feadf60e0ac
wcs <1103409364@qq.com>
2022-01-29 15:39:27 +0800

    SpringBoot 关闭启动 logo

--- springboot/demo01/src/main/java/com/demo/springboot/Demo01Application.java
+++ springboot/demo01/src/main/java/com/demo/springboot/Demo01Application.java
@@ -1,6 +1,7 @@
 package com.demo.springboot;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo01Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(Demo01Application.class, args);
+    // SpringApplication.run(Demo01Application.class, args);
+    // 获取启动入口 Demo01Application 类，设置logo关闭，启动run
+    SpringApplication springApplication = new SpringApplication(Demo01Application.class);
+    springApplication.setBannerMode(Banner.Mode.OFF);
+    springApplication.run(args);
   }
 }
 // SpringBoot项目必须放在Application类同级目录或者下级目录
