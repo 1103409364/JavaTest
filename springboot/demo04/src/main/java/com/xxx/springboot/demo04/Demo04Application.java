0ff067380fc4e7b579ff551e8503ccf0ef9a1175
wcs <1103409364@qq.com>
2022-01-29 15:18:18 +0800

    SpringBoot 创建Java工程，第二种方式

--- springboot/demo04/src/main/java/com/xxx/springboot/demo04/Demo04Application.java
+++ springboot/demo04/src/main/java/com/xxx/springboot/demo04/Demo04Application.java
@@ -1,24 +1,41 @@
 package com.xxx.springboot.demo04;
 
 import com.xxx.springboot.demo04.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
-@SpringBootApplication
-public class Demo04Application {
+//@SpringBootApplication
+//public class Demo04Application {
+//
+//  public static void main(String[] args) {
+//    //  Springboot 程序启动后，返回值是ConfigurableApplicationContext,它也是一个Spring 容器
+//    //  它其实相当于原来Spring容器中后动容器ClasspathXmLApplicationContext
+//    ConfigurableApplicationContext applicationContext = SpringApplication.run(Demo04Application.class, args);
+//
+//    // 从spring容器中获取指定bean对象
+//    StudentService studentService = (StudentService) applicationContext.getBean("studentServiceImpl");
+//
+//    // 调用业务方法
+//    String sayHello = studentService.sayHello();
+//    System.out.println(sayHello);
+//  }
+//
+//}
+@SpringBootApplication //实现CommandLineRunner
+public class Demo04Application implements CommandLineRunner {
+  @Autowired // 注入
+  private StudentService studentService;
 
   public static void main(String[] args) {
-    //  Springboot 程序启动后，返回值是ConfigurableApplicationContext,它也是一个Spring 容器
-    //  它其实相当于原来Spring容器中后动容器ClasspathXmLApplicationContext
-    ConfigurableApplicationContext applicationContext = SpringApplication.run(Demo04Application.class, args);
+    SpringApplication.run(Demo04Application.class, args);
+  }
 
-    // 从spring容器中获取指定bean对象
-    StudentService studentService = (StudentService) applicationContext.getBean("studentServiceImpl");
-
+  @Override // 重写run方法
+  public void run(String... args) throws Exception {
     // 调用业务方法
     String sayHello = studentService.sayHello();
     System.out.println(sayHello);
   }
-
 }
