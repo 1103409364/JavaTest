519214976d4313ad23894f1dfb632f28c030aee5
wcs <1103409364@qq.com>
2022-01-31 11:02:50 +0800

    SpringBoot集成logback日志

--- springboot/demo01/src/main/java/com/demo/springboot/controller/StudentController.java
+++ springboot/demo01/src/main/java/com/demo/springboot/controller/StudentController.java
@@ -2,12 +2,14 @@ package com.demo.springboot.controller;
 
 import com.demo.springboot.model.Student;
 import com.demo.springboot.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class StudentController {
   // 注入业务层，调到业务层接口实现类
   @Autowired
@@ -16,6 +18,7 @@ public class StudentController {
   @RequestMapping(value = "/student")
   public @ResponseBody
   Object student(Integer id) {
+    log.info("通过id查询学生");
     return studentService.queryStudentById(id);
   }
 
